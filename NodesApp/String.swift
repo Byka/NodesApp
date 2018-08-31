403feb98b31e54799f68014a06991d7891ad59f0
System Administrator <root@JNETs-Mac-mini.local>
2018-08-31 16:35:15 +0530

    Final commit

--- NodesApp/String.swift
+++ NodesApp/String.swift
@@ -8,6 +8,65 @@
 
 import UIKit
 
-class String: NSObject {
 
+    extension Path {
+        var array: [Node] {
+            var array: [Node] = [self.node]
+            
+            var iterativePath = self
+            while let path = iterativePath.previousPath {
+                array.append(path.node)
+                
+                iterativePath = path
+            }
+            
+            return array
+        }
+    }
+
+
+class Node {
+    var visited = false
+    var connections: [Connection] = []
 }
+
+class Connection {
+    public let to: Node
+    public let weight: Int
+    
+    public init(to node: Node, weight: Int) {
+        assert(weight >= 0, "weight has to be equal or greater than zero")
+        self.to = node
+        self.weight = weight
+    }
+}
+
+class Path {
+    public let cumulativeWeight: Int
+    public let node: Node
+    public let previousPath: Path?
+    
+    init(to node: Node, via connection: Connection? = nil, previousPath path: Path? = nil) {
+        if
+            let previousPath = path,
+            let viaConnection = connection {
+            self.cumulativeWeight = viaConnection.weight + previousPath.cumulativeWeight
+        } else {
+            self.cumulativeWeight = 0
+        }
+        
+        self.node = node
+        self.previousPath = path
+    }
+}
+
+// **** EXAMPLE BELOW ****
+class MyNode: Node {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
+
--- NodesApp/ViewController.swift
+++ NodesApp/ViewController.swift
@@ -10,16 +10,111 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func actionOne(_ sender: Any) {
+        let nodeA = MyNode(name: "A")
+        let nodeB = MyNode(name: "B")
+        let nodeC = MyNode(name: "C")
+        let nodeD = MyNode(name: "D")
+        let nodeE = MyNode(name: "E")
+        let nodeF = MyNode(name: "F")
+        
+        nodeA.connections.append(Connection(to: nodeB, weight: 1))
+        nodeB.connections.append(Connection(to: nodeC, weight: 3))
+        nodeC.connections.append(Connection(to: nodeD, weight: 1))
+        nodeB.connections.append(Connection(to: nodeE, weight: 1))
+        nodeE.connections.append(Connection(to: nodeC, weight: 1))
+        nodeC.connections.append(Connection(to: nodeF, weight: 1))
+        
+        let sourceNode = nodeA
+        let destinationNode = nodeC
+        
+        var path = shortestPath(source: sourceNode, destination: destinationNode)
+        
+        if let succession: [String] = path?.array.reversed().flatMap({ $0 as? MyNode}).map({$0.name}) {
+            print("🏁 Quickest path: \(succession)")
+        } else {
+            print("💥 No path between \(sourceNode.name) & \(destinationNode.name)")
+        }
+    }
+    
+    @IBAction func actionTwo(_ sender: Any) {
+        let nodeA = MyNode(name: "A")
+        let nodeB = MyNode(name: "B")
+        let nodeC = MyNode(name: "C")
+        let nodeD = MyNode(name: "D")
+        let nodeE = MyNode(name: "E")
+        let nodeF = MyNode(name: "F")
+        
+        nodeA.connections.append(Connection(to: nodeB, weight: 1))
+        nodeB.connections.append(Connection(to: nodeC, weight: 3))
+        nodeC.connections.append(Connection(to: nodeD, weight: 1))
+        nodeB.connections.append(Connection(to: nodeE, weight: 1))
+        nodeE.connections.append(Connection(to: nodeC, weight: 1))
+        nodeC.connections.append(Connection(to: nodeF, weight: 1))
+        
+        let sourceNode = nodeA
+        let destinationNode = nodeF
+        
+        var path = shortestPath(source: sourceNode, destination: destinationNode)
+        
+        if let succession: [String] = path?.array.reversed().flatMap({ $0 as? MyNode}).map({$0.name}) {
+            print("🏁 Quickest path: \(succession)")
+        } else {
+            print("💥 No path between \(sourceNode.name) & \(destinationNode.name)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+       
+        
+        
+       
+        
+        
+
+        
     }
+    
+    func shortestPath(source: Node, destination: Node) -> Path? {
+        var frontier: [Path] = [] {
+            didSet { frontier.sort { return $0.cumulativeWeight < $1.cumulativeWeight } } // the frontier has to be always ordered
+        }
+        
+        frontier.append(Path(to: source)) // the frontier is made by a path that starts nowhere and ends in the source
+        
+        while !frontier.isEmpty {
+            let cheapestPathInFrontier = frontier.removeFirst() // getting the cheapest path available
+            guard !cheapestPathInFrontier.node.visited else { continue } // making sure we haven't visited the node already
+            
+            if cheapestPathInFrontier.node === destination {
+                return cheapestPathInFrontier // found the cheapest path 😎
+            }
+            
+            cheapestPathInFrontier.node.visited = true
+            
+            for connection in cheapestPathInFrontier.node.connections where !connection.to.visited { // adding new paths to our frontier
+                frontier.append(Path(to: connection.to, via: connection, previousPath: cheapestPathInFrontier))
+            }
+        } // end while
+        return nil // we didn't find a path 😣
+    }
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
 
 }
+
 
